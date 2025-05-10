6c6238d903ff6190a6ff438e87a895c9425a793d
gabri <gabrielmarliere2005@gmail.com>
2025-05-10 17:45:22 -0300

    [FEAT] Começcando o modulo de processamento de video

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppService } from './app.service';
 import { ImageUpscaleModule } from './image-upscale/image-upscale.module';
 import { ImageNoiseModule } from './image-noise/image-noise.module';
 import { ImageDownscaleModule } from './image-downscale/image-downscale.module';
+import { ImageDetectionModule } from './image-detection/image-detection.module';
 
 @Module({
-  imports: [ImageUpscaleModule, ImageNoiseModule, ImageDownscaleModule],
+  imports: [ImageUpscaleModule, ImageNoiseModule, ImageDownscaleModule, ImageDetectionModule],
   providers: [AppService],
 })
 export class AppModule {}
